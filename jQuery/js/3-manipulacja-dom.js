5f73b51b32a0d8aba8f523d10769adf839ec9441
Kamil Obuchiewicz <kam.obi@wp.pl>
2017-10-28 14:40:13 +0200

    jQuery-podstawy

---
+++ jQuery/js/3-manipulacja-dom.js
@@ -0,0 +1,61 @@
+'use strict';
+$ (function(){
+//   var pobierzText = $('#second');
+//    console.log(pobierzText.text());
+    
+//    zmiana tekstu nagłówka 
+    
+//    pobierzText.text('Zmieniony tekst');
+//    console.log(pobierzText.text());
+    
+//    var body = $('body');
+//    
+//    console.log(body.html('<p>Nowy paragraf</p>'));
+    
+//    var przykladAppend = $('.paragrafnext');
+//    przykladAppend.append('Appendujemy text');
+//    
+//     var przykladAfter = $('.paragrafnext');
+//    przykladAppend.after('<span id=\'spanText\'>Affterujemy text</span>');
+//    
+//     var przykladPrepend = $('.paragrafnext');
+//    przykladAppend.prepend('Prependujemy text |');
+//    
+//     var przykladBefore = $('.paragrafnext');
+//    przykladAppend.before('Beforujemy text |');
+//    
+//    var emptyPrzyklad = $('.paragrafnext');
+//    emptyPrzyklad.empty();
+//    
+//    var removePrzyklad = $('.paragrafnext');
+//    removePrzyklad.remove();
+//    
+//    var cytatKrystiana = 'Krystian powiedział: "powiedział coś mądrego ;) " ';
+//    console.log(cytatKrystiana);
+//    
+    
+//    zmieniamy własciwości z css za pomocą jQuery !
+//    var zmienKolor = $('#first');
+//    
+//    zmienKolor.css('color', 'green');
+//    zmienKolor.css({'color':'blue', 'font-size' : '4em'})
+    
+//    var inputValue = $('#inputValue');
+//    
+//    inputValue.val('Jestem tekstem ;)');
+//    console.log(inputValue.val());
+    
+//    Zmiana za pomocą jQuery
+//    var drugiNaglowek =$('#second');
+//    drugiNaglowek.addClass('blue');              
+//    
+//     drugiNaglowek.removeClass('blue');
+//    
+//    
+////     Zmiana za pomocą czystego JavaScript
+//    var drugiNaglowek = document.getElementById('second');   
+//    drugiNaglowek.classList.add('blue');
+});
+
+
+
---
+++ jQuery/js/4-find-each.js
@@ -0,0 +1,13 @@
+'use strict';
+$ (function(){
+//    $('body').find('p').eq(2).css('color', 'blue');
+    $('.paragraf').each(function (index, element){
+        if(index != 1) {
+            $(this).css('background-color', 'green');
+        }
+        console.log('Element:' + element, index);
+        
+        
+    });
+    
+});
