b7a5fc2b0098036d0a1550d03e736a67a99574fa
Kamil Obuchiewicz <kam.obi@wp.pl>
2017-11-18 11:28:44 +0100

    powtorka-JSON

---
+++ sass-gulp/app/js/sass.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*Przyklady z JSON */
+
+//$.getJSON('../package.json', function(data){
+//    
+////   var autor = data.author;
+//    wstaw(autor);
+//});
+//
+//function wstaw(autor) {
+//    $('span').append(autor);
+//}
+
+//-------------------------------------------------------------
+//SASS
