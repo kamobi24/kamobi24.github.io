b24a1d791278823b5d3d191a156a6b215cda1577
Kamil Obuchiewicz <kam.obi@wp.pl>
2017-11-22 21:33:33 +0100

    poprawa adresu

--- Ajax-zadanie-domowe/js/ajax.js
+++ Ajax-zadanie-domowe/js/ajax.js
@@ -1,4 +1,4 @@
-$.getJSON("https://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108", function (data) {
+$.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
     $("button").click(function () {
         $('button').after($('<div id="dane-programisty"></div>'));
         $("#dane-programisty").text(data.imie + ' ' + data.nazwisko + ' ' + data.zawod + ' ' + data.firma + ' ');
