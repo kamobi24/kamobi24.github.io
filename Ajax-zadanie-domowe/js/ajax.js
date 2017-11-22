2f5cc4f4bfa6c3716c4f6ed69c43e1c5932613f3
Kamil Obuchiewicz <kam.obi@wp.pl>
2017-11-22 21:32:02 +0100

    zmiana strony

--- Ajax-zadanie-domowe/js/ajax.js
+++ Ajax-zadanie-domowe/js/ajax.js
@@ -1,4 +1,4 @@
-$.getJSON("http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108", function (data) {
+$.getJSON("https://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108", function (data) {
     $("button").click(function () {
         $('button').after($('<div id="dane-programisty"></div>'));
         $("#dane-programisty").text(data.imie + ' ' + data.nazwisko + ' ' + data.zawod + ' ' + data.firma + ' ');
