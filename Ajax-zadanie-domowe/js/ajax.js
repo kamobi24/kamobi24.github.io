2a3a5a222e18ccbf3b717e5256ae41149f740c49
Kamil Obuchiewicz <kam.obi@wp.pl>
2017-11-22 21:41:11 +0100

    zmiana przycisku

--- Ajax-zadanie-domowe/js/ajax.js
+++ Ajax-zadanie-domowe/js/ajax.js
@@ -1,5 +1,8 @@
-$.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
-    $("button").click(function () {
+$("button").click(function () {
+
+    $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
+
+        console.log(data)
         $('button').after($('<div id="dane-programisty"></div>'));
         $("#dane-programisty").text(data.imie + ' ' + data.nazwisko + ' ' + data.zawod + ' ' + data.firma + ' ');
     })
